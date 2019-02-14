c0c8be4f723446e00b6b4f511054ac699ee39ad4
KopylovVitalik <vitalik.kopylov@gmail.com>
2019-02-14 13:10:47 +0200

    card add button fixed

--- src/js/_add-collection-btn.js
+++ src/js/_add-collection-btn.js
@@ -1,7 +1,5 @@
 $('.js-add-collection-btn').each((index, el) => {
   const btn = $(el);
-  const anchor = btn.closest('a');
-  anchor.on('click', e => e.preventDefault());
   btn.on('click', e => {
     btn.addClass('btn_blue').removeClass('btn_light-grey');
     btn.find('.btn__text').text('Сохранено');
